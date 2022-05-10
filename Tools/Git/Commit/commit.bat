ebd6449969961e0d606e9e0990ae3b756b3dd007
VyacheslavSaniuk <saniuk.vyacheslav.97@gmail.com>
2022-05-10 10:37:59 +0300

    Добавление Git Push интрумента

--- Tools/Git/Commit/commit.bat
+++ Tools/Git/Commit/commit.bat
@@ -6,11 +6,11 @@ git status
 
 REM // Add all new/modified files to commit
 ECHO Добавление всех изменений:
-git add -A
+git add -A >> NUL
 
 REM // Commit modified files
 FOR /F "delims=" %%i IN ('cscript //nologo Tools\Git\Commit\message_input.vbs') DO set commit_message=%%i
 ECHO Коммит с сообщением: %commit_message%
-git commit -m "%commit_message%"
+git commit -m "%commit_message%" >> NUL
 
 ECHO Завершение комита.
---
+++ Tools/Git/Push/push.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+ECHO Отправка изменений на GitHub:
+git push origin main
+ECHO Отправка изменений завершена.
